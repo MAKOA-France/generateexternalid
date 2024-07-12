e8781c555920ff38557a60a5047edab3215fb2bd
sitraka <sitraka@makoa.fr>
2024-07-12 09:29:08 +0200

    AB: Check and clean variable to null value

--- CRM/Generateexternalid/Utils.php
+++ CRM/Generateexternalid/Utils.php
@@ -27,21 +27,25 @@ class CRM_Generateexternalid_Utils {
 
     }
     public static function setExternalID($params){
+        // Get variable and clean string
         $id = $params['id'];
-        $external_identifier = $params['external_identifier'];
-        $last_name = $params['last_name'];
-        $first_name = $params['first_name'];
-        $middle_name = $params['middle_name'];
-
-        $str = $last_name.$first_name.$middle_name;
+        $external_identifier =  self::checkstring($params['external_identifier']);
+        $last_name =  self::checkstring($params['last_name']);
+        $first_name =  self::checkstring($params['first_name']);
+        $middle_name = self::checkstring($params['middle_name']);       
+            
+        // Build the external identifiant
+        $str = $last_name.$first_name;
+        if (!is_null($middle_name)) $str .= $middle_name;
         $strFormatted = self::remove_accents_and_special_chars($str,"");
         $strFormatted = strtolower($strFormatted);
 
+        // if external identifier is different to this on bdd
         if( $external_identifier != $strFormatted){
             // update external identifier
             $api = \Civi\Api4\Contact::update(FALSE);
             $api->addValue('first_name',$first_name);
-            if (!is_null($middle_name)) $api->addValue('middle_name', $middle_name);
+            if (!empty($middle_name)) $api->addValue('middle_name', $middle_name);
             $api->addValue('last_name', $last_name);
             $api->addValue('external_identifier', $strFormatted);
             $api->addWhere('id', '=', $id);
@@ -95,7 +99,9 @@ class CRM_Generateexternalid_Utils {
         return $str;
     }
 
-    public static function buildExternal($str){
+    public static function checkstring($str){
 
+        if( strtolower($str) == 'null') $str = '';
+        return $str;
     }
 }
